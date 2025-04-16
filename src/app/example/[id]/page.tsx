d855635965d8ab99f9de0100fd1fa2642d7e2a78
James Grieve <jamesonrgrieve@gmail.com>
2025-04-15 19:40:37 -0600

    Dynamic paths;

--- src/app/example/[id]/page.tsx
+++ src/app/example/[id]/page.tsx
@@ -3,9 +3,10 @@
 import Link from "next/link"; // Server-Side
 import { useRouter } from "next/navigation"; // Client-Side
 import React from "react";
-export default function ExamplePage({ searchParams }) {
+export default function ExamplePage({ params, searchParams }) {
   const router = useRouter();
   const { name } = React.use(searchParams);
+  const { id } = React.use(params);
   return (
     <>
       <h1>Hello, Again, World!</h1>
@@ -18,6 +19,7 @@ export default function ExamplePage({ searchParams }) {
       >
         Go Home Client
       </button>
+      <p>ID Slug: {id}</p>
       <p>Name Search Param: {name}</p>
     </>
   );
