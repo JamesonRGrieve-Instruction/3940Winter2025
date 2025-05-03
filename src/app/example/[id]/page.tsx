024664810b37772e5e0292af8c013926fd0d6f3b
James Grieve <jamesonrgrieve@gmail.com>
2025-05-02 19:56:29 -0600

    Generate static params;

--- src/app/example/[id]/page.tsx
+++ src/app/example/[id]/page.tsx
@@ -31,3 +31,8 @@ export function generateStaticParams() {
   }));
 }
 export const dynamicParams = false;
+export async function generateMetadata({ params }) {
+  return {
+    title: posts[(await params).id].content,
+  };
+}
